9e615d96ba66d3b19a63b8a07b325ca915184870
анна <lekomtseva394@inbox.ru>
2023-06-04 00:06:53 +0500

    encryption, decryption and cryptanalysis

---
+++ Analiz.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enigma_Form
+{
+    internal class Analiz
+    {
+
+        string aft_ref;
+        string aft_rot;
+        string aft_kom;
+        string alfabhet;
+        public char[,] otvet_rotor;
+
+        public char[] analiz_ref;
+        public char[] analiz_kom;
+        public char[] analiz_rot;
+
+        string text;
+
+        public Analiz(string aft_ref, string aft_rot, string aft_kom, string alfabhet, string text)
+        {
+            this.aft_ref = Deleter(aft_ref);
+            this.aft_rot = Deleter(aft_rot);
+            this.aft_kom = Deleter(aft_kom);
+            this.alfabhet = alfabhet;
+            this.text = Deleter(text).ToUpper();
+
+            otvet_rotor = new char[alfabhet.Length, aft_ref.Length];
+        }
+        string Deleter(string str)
+        {
+            string new_str = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    new_str = new_str;
+                }
+                else
+                {
+                    new_str += str[i];
+                }
+
+            }
+            return new_str;
+        }
+        
+        public double[] Chastota(string str)
+        {
+            double[] shet = new double[alfabhet.Length];
+            double prov = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < alfabhet.Length; j++)
+                {
+                    if (str[i] == alfabhet[j])
+                    {
+                        shet[j] += 1;
+                    }
+
+                }
+            }
+            for (int i = 0; i < shet.Length; i++)
+            {
+                shet[i] = (shet[i] / aft_kom.Length);
+                prov = prov + shet[i];
+            }
+            return shet;
+        }
+
+        public char[] AnalizReflector()
+        {
+            int[] an_ref = new int[alfabhet.Length];
+            char[] anref_ch = new char[alfabhet.Length];
+            double[] text_ver = new double[alfabhet.Length];
+            double[] ref_ver = new double[alfabhet.Length];
+
+            text_ver = Chastota(text);
+            ref_ver = Chastota(aft_ref);
+
+            int k = 0;
+            
+                for (int i = 0; i < text_ver.Length; i++)
+                {
+                    for (int j = 0; j < ref_ver.Length; j++)
+                    {
+                        if (text_ver[i] == ref_ver[j])
+                        {
+                            an_ref[i] = j;
+                        }
+                    }
+                }
+        
+            for (int i = 0; i < an_ref.Length; i++)
+            {
+                k = an_ref[i];
+                anref_ch[i] = alfabhet[k];
+            }
+
+            return anref_ch;
+        }
+        public char[] AnalizRotor()
+        {
+            int n = 0; int l = 0;
+            for (int i = 0; i < otvet_rotor.GetLength(0); i++)
+            {
+                for (int j = 0; j < otvet_rotor.GetLength(1); j++)
+                {
+                    otvet_rotor[i, j] = '-';
+                }
+            }
+            for (int i = 0; i < aft_ref.Length; i++)
+            {
+                for (int j = 0; j < alfabhet.Length; j++)
+                {
+                    if ((aft_ref[i] == alfabhet[j]) & (l < otvet_rotor.GetLength(1)))
+                    {
+                        n = j;
+
+                        otvet_rotor[n, l] = aft_rot[i];
+
+
+                        l++;
+                    }
+                }
+            }
+            int c = otvet_rotor.GetLength(1) / alfabhet.Length; int h = 1;
+            while (h < c)
+            {
+                for (int i = 0; i < otvet_rotor.GetLength(0); i++)
+                {
+                    for (int j = 0; j < otvet_rotor.GetLength(1); j++)
+                    {
+                        if ((j >= alfabhet.Length * h) && (otvet_rotor[i, j - alfabhet.Length * h] == '-'))
+                        {
+                            otvet_rotor[i, j - alfabhet.Length * h] = otvet_rotor[i, j];
+                        }
+
+                    }
+                }
+                h++;
+            }
+
+           
+            for (int i = 0; i < otvet_rotor.GetLength(0); i++)
+            {
+                for (int j = 0; j < alfabhet.Length; j++)
+                {
+                    if (otvet_rotor[i, j] == '-')
+                    {
+                        otvet_rotor[i, j] = otvet_rotor[i + 1, j + 1];
+                    }
+                }
+            }
+            
+            
+            char[] otvet = new char[alfabhet.Length];
+            for (int i = 0; i < otvet.Length; i++)
+            {
+                otvet[i] = otvet_rotor[i, 0];
+            }
+
+            return otvet;
+        }
+
+        public char[] AnalizKommutator()
+        {
+            char[] ankom_ch = new char[alfabhet.Length];
+
+            for (int i = 0; i < aft_rot.Length; i++)
+            {
+                for (int j = 0; j < alfabhet.Length; j++)
+                {
+                    if (aft_rot[i] == alfabhet[j])
+                    {
+                        ankom_ch[j] = aft_kom[i];
+                    }
+                }
+            }
+
+            return ankom_ch;
+        }
+
+        public void AnalizAll()
+        {
+            analiz_ref = AnalizReflector();
+            analiz_rot = AnalizRotor();
+            analiz_kom = AnalizKommutator();
+        }
+
+    }
+}
+
---
+++ Enigma.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enigma_Form
+{
+    internal class Enigma
+    {
+        string alf;
+        string reflect;
+        string kommut;
+        string rotor_start;
+        char[] alf_mas;
+        char[] ref_mas;
+        char[] kom_mas;
+        char[] rotstart_mas;
+        char[] rotend_mas;
+
+        public string aft_ref, aft_rot, aft_kom;
+        public Enigma(string alf, string reflect, string kommut, string rotor_start)
+        {
+            this.alf = alf;
+            this.reflect = reflect;
+            this.kommut = kommut;
+            this.rotor_start = rotor_start;
+            alf_mas = convert(alf);
+            ref_mas = convert(reflect);
+            kom_mas = convert(kommut);
+            rotstart_mas = convert(rotor_start);
+            rotend_mas = new char[rotstart_mas.Length];
+        }
+        public char[] convert(string stroka)
+        {
+            char[] arr = new char[stroka.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = stroka[i];
+            }
+            return arr;
+        }
+
+        string reflect_method(string text)
+        {
+            string text_after_ref = "";
+            char[] text_mas = convert(text);
+            char[] after_mas = new char[text_mas.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                
+                for (int j = 0; j < alf.Length; j++)
+                {
+                    if (text_mas[i] == alf_mas[j])
+                    {
+                        after_mas[i] = ref_mas[j];
+                        text_after_ref += ref_mas[j];
+                    }
+                    
+                }
+            }
+            return text_after_ref;
+        }
+
+        string kommut_method(string text)
+        {
+            string text_after_kom = "";
+            char[] text_mas = convert(text);
+            char[] after_mas = new char[text_mas.Length];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < alf.Length; j++)
+                {
+                    if (text_mas[i] == alf_mas[j])
+                    {
+                        after_mas[i] = kom_mas[j];
+                        text_after_kom += kom_mas[j];
+                    }
+                    
+                }
+            }
+            return text_after_kom;
+        }
+
+        char[] sdvig(char[] mas)
+        {
+            char[] mas_sdvig = new char[mas.Length];
+            char last = mas[mas.Length - 1];
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                mas_sdvig[i + 1] = mas[i];
+            }
+            mas_sdvig[0] = last;
+            return mas_sdvig;
+        }
+        char[] sdvig_obr(char[] mas)
+        {
+            char[] mas_sdvig = new char[mas.Length];
+            char first = mas[0];
+            for (int i = mas.Length - 1; i > 0; i--)
+            {
+                mas_sdvig[i - 1] = mas[i];
+            }
+            mas_sdvig[mas.Length - 1] = first;
+            return mas_sdvig;
+        }
+
+        string rotor_method(string text)
+        {
+            string text_after_rotor = "";
+            char[] text_mas = convert(text);
+            char[] rotor_next = rotstart_mas;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+               
+                for (int j = 0; j < rotor_start.Length; j++)
+                {
+                    if (text_mas[i] == alf_mas[j])
+                    {
+                        text_after_rotor += rotor_next[j];
+                    }
+                   
+                }
+                rotor_next = sdvig(rotor_next);
+
+            }
+            rotend_mas = rotor_next;
+            return text_after_rotor;
+        }
+
+        string rotor_method_obr(string text)
+        {
+            string text_after_rotor = "";
+            char[] text_mas = convert(text);
+            char[] rotor_next = rotstart_mas;
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < alf_mas.Length; j++)
+                {
+                    if (text[i] == rotor_next[j])
+                    {
+                        text_after_rotor += alf_mas[j];
+                    }
+                }
+                rotor_next = sdvig(rotor_next);
+            }
+            return text_after_rotor;
+        }
+
+        public string Enigma_meth(string text)
+        {
+            Console.WriteLine("ЗАШИФРОВКА");
+            text = text.ToUpper();
+            aft_ref = reflect_method(text);
+            Console.WriteLine("Результат после прохождения через рефлектор: ");
+            Console.WriteLine(aft_ref);
+            aft_rot = rotor_method(aft_ref);
+            Console.WriteLine("Результат после прохождения через ротор: ");
+            Console.WriteLine(aft_rot);
+            aft_kom = kommut_method(aft_rot);
+            Console.WriteLine("Результат после прохождения через коммутатор: ");
+            Console.WriteLine(aft_kom);
+            Console.WriteLine("-----------------------------------------------------");
+
+            return aft_kom;
+        }
+
+        public string Enigma_bigText(string text)
+        {
+            Console.WriteLine("ЗАШИФРОВКА");
+            text = text.ToUpper();
+            aft_ref = reflect_method(text);
+            //Console.WriteLine("Результат после прохождения через рефлектор: ");
+            aft_rot = rotor_method(aft_ref);
+            //Console.WriteLine("Результат после прохождения через ротор: ");
+            aft_kom = kommut_method(aft_rot);
+            Console.WriteLine("DONE");
+
+            return aft_kom;
+        }
+
+        public string Enigma_back(string text)
+        {
+            Console.WriteLine("ДЕШИФРОВКА");
+            string aft_kom1 = kommut_method(text);
+            Console.WriteLine("Результат после прохождения через коммутатор: ");
+            Console.WriteLine(aft_kom1);
+            string aft_rot1 = rotor_method_obr(aft_kom1);
+            Console.WriteLine("Результат после прохождения через ротор: ");
+            Console.WriteLine(aft_rot1);
+            string aft1 = reflect_method(aft_rot1);
+            Console.WriteLine("Результат после прохождения через рефлектор: ");
+            Console.WriteLine(aft1);
+            Console.WriteLine("-----------------------------------------------------");
+
+            return aft1;
+        }
+
+        public string Enigma_back_bigText(string text)
+        {
+            Console.WriteLine("ДЕШИФРОВКА");
+            string aft_kom1 = kommut_method(text);
+            //Console.WriteLine("Результат после прохождения через коммутатор: ");
+            string aft_rot1 = rotor_method_obr(aft_kom1);
+            //Console.WriteLine("Результат после прохождения через ротор: ");
+            string aft1 = reflect_method(aft_rot1);
+            Console.WriteLine("DONE");
+
+            return aft1;
+        }
+    }
+}
+
